08a9f01bacaeb04cbfa4f30f779333f5c0537232
William Engman <williamengman24@gmail.com>
2026-01-13 12:02:51 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+let greeting = "yo";
+
+function App() {
+
+  return (
+    <>
+    <h1>Hello World</h1>
+
+    <p>Test</p>
+
+    <p>{greeting}</p>
+    </>
+  )
+}
+
+export default App
